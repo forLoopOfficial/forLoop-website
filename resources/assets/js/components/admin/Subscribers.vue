a0478b152398b4467c7ffd885dfa29e9c803ab54
Wale Martins <madarasage@gmail.com>
2017-02-12 10:17:08 +0100

    Features and Bug fixes

---
+++ resources/assets/js/components/admin/Subscribers.vue
@@ -0,0 +1,50 @@
+<template>
+
+  <!-- page content -->
+  <div class="right_col" role="main">
+    <div class="col-md-12">
+      <div class="x_panel">
+        <div class="x_title">
+          <h2>Subscribers</h2>
+          <ul class="nav navbar-right panel_toolbox">
+            <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+            </li>
+          </ul>
+          <div class="clearfix"></div>
+        </div>
+        <div class="x_content">
+          <ol>
+            <li v-for="subscriber in subscribers">
+              {{subscriber['.value']}}
+            </li>
+            <li v-if="!subscribers">No Subscribers</li>
+          </ol>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- /page content -->
+
+</template>
+
+
+<script>
+import firebase from 'firebase';
+
+const subscribersRef = firebase.database().ref('subscribers');
+export default {
+  name: 'Subscribers',
+  data () {
+    return {
+    }
+  },
+  firebase: {
+    subscribers: subscribersRef
+  }
+}
+
+</script>
+
+<style>
+
+</style>
---
+++ resources/assets/js/components/site/AddSubscriber.vue
@@ -0,0 +1,46 @@
+<template>
+    <!-- Jumbotron -->
+    <section class="jumbotron">
+        <div class="container">
+            <h2 class="jumbotron__title jumbotron__title--2">Get notified about Upcoming Meetups</h2>
+            <div class="jumbotron__action jumbotron__action--maxsize">
+                <form v-on:submit.prevent="addSubscriber" class="form">
+                    <div class="outer-container">
+                        <div class="input-section">
+                            <input v-model="subscriber" type="email" class="form__input" placeholder="Email Address" required/>
+                        </div>
+                        <div class="submit-button">
+                            <button type="submit" class="btn btn--primary btn--block">Notify Me</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </section>
+</template>
+
+
+<script>
+import firebase from 'firebase';
+
+const subscribersRef = firebase.database().ref('subscribers');
+export default {
+  name: 'AddSubscriber',
+  data () {
+    return {
+      subscriber: ''
+    }
+  },
+  methods: {
+    addSubscriber (e) {
+      subscribersRef.push(this.subscriber);
+      alert("You have been subscribed")
+    }
+  }
+}
+
+</script>
+
+<style>
+
+</style>
