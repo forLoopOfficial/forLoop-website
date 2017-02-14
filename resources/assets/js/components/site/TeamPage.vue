e4bd3b4372bbe840feca532ec7ded5d91d0b72bf
Wale Martins <madarasage@gmail.com>
2017-02-14 21:43:36 +0100

    Fix default box on team pag

--- resources/assets/js/components/site/TeamPage.vue
+++ resources/assets/js/components/site/TeamPage.vue
@@ -14,18 +14,6 @@
       <section class="teams">
           <div class="team__list">
               <!-- Teams List Item -->
-              <div class="team__list__item position-relative" style="background-image: url('img/1.png')">
-                  <div class="team__list__item__container">
-                      <div class="team__list__item__name">Imogie Mubarak</div>
-                      <div class="team__list__item__handle">
-                          <div class="handle__icon">
-                              <svg class="icon-social-twitter icon-sm"><use xlink:href="img/icons.svg#icon-social-twitter"></use></svg>
-                          </div>
-                          <div class="handle__text">@imogiemubarak</div>
-                      </div>
-                  </div>
-              </div>
-              <!-- Teams List Item -->
               <div v-for="member in team_page.members" class="team__list__item position-relative" :style="{ backgroundImage: 'url(\''+member.profile_image+'\')'}">
                   <div class="team__list__item__container">
                       <div class="team__list__item__name">{{member.name}}</div>
